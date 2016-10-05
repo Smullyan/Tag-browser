16c2b521e98958ded3bf7085f0eb8586d1654e43
Aulia Widyaputra <aulia.widyaputra@gmail.com>
2016-10-06 01:13:48 +0700

    Created function to get file path.

--- tag.py
+++ tag.py
@@ -1,12 +1,25 @@
 #! /usr/bin/python
 
-import subprocess
+import os
+
 
 def _get_filepath(filename):
-    
+    # using os.path.exists rather than os.path.isfile because
+    # os.path.exists checks directories also
+    # requires use of quotes (" ") in filename to execute
+    # the return value will then be used in a sorted data file.
+    if os.path.exists(filename) is True:
+        return os.path.abspath(filename)
+    else:
+        print "File does not exist."
+        # I think this is correct
+        return 0
 
+
+"""
 def _ask_for_comments:
 
 def _store_tags:
 
 def tag:
+"""
