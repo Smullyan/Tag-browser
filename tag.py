ae5f45fb98999c4c7b5a1e63b1d8573f0df4ac31
Aulia Widyaputra <aulia.widyaputra@gmail.com>
2016-10-07 21:21:21 +0700

    Added more ideas on how to implement tag system. Need to figure out how to identify the '#' symbol from a string.

--- tag.py
+++ tag.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#!/usr/bin/python
 
 import os
 
@@ -8,7 +8,7 @@ def _get_filepath(filename):
     # os.path.exists checks directories also
     # requires use of quotes (" ") in filename to execute
     # the return value will then be used in a sorted data file.
-    if os.path.exists(filename) is True:
+    if os.path.exists(filename) is True:        
         return os.path.abspath(filename)
     else:
         print "File does not exist."
@@ -16,7 +16,22 @@ def _get_filepath(filename):
         return 0
 
 
+def _ask_for_comment():
+    comment = raw_input(">> ")
+    return comment
+
+
+def _store_data():
+
+    # I'm thinking of storing filename, file path, comment and tags into an output file
+    # Make code to be able to identify '#' symbol and mark word after that
+    # Store the tag to output file above and maybe another output file to do metadata analysis
+    # Then add all those functions to a single function.
+
 """
+Maybe use Tuples to record data?
+That said, Tuples can't be updated, so maybe that's a bad way to store data.
+
 def _ask_for_comments:
 
 def _store_tags:
